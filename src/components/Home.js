db7430db3df6b05408e3df19ea3ad0947c07dc7e
Darrell-Rafa <drafa0609@gmail.com>
2022-12-10 21:57:26 +0700

    add sections

---
+++ src/components/Home.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        model="Model 3"
+        desc="Schedule a Test Drive"
+        img="model-3.jpg"
+        lBtnText="custom order"
+        rBtnText="existing inventory"
+      />
+      <Section
+        model="Model Y"
+        desc="Schedule a Test Drive"
+        img="model-y.jpg"
+        lBtnText="custom order"
+        rBtnText="existing inventory"
+      />
+      <Section
+        model="Model S"
+        desc="Schedule a Test Drive"
+        img="model-s.jpg"
+        lBtnText="custom order"
+        rBtnText="existing inventory"
+      />
+      <Section
+        model="Model X"
+        desc="Schedule a Test Drive"
+        img="model-x.jpg"
+        lBtnText="custom order"
+        rBtnText="existing inventory"
+      />
+      <Section
+        model="Solar Panels"
+        desc="Lowest Cost Solar Panels in America"
+        img="solar-panel.jpg"
+        lBtnText="order now"
+        rBtnText="learn more"
+      />
+      <Section
+        model="Solar Roof"
+        desc="Produce Clean Energy From Your Roof"
+        img="solar-roof.jpg"
+        lBtnText="order now"
+        rBtnText="learn more"
+      />
+      <Section model="Accessories" img="accessories.jpg" lBtnText="shop now" />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
---
+++ src/components/Section.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = (prop) => {
+  return (
+    <Wrap bgImg={prop.img}>
+      <ItemText>
+        <h1 style={{ fontSize: "3rem" }}>{prop.model}</h1>
+        <p style={{ marginTop: "8px" }}>{prop.desc}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton rBtn={prop.rBtnText}>{prop.lBtnText}</LeftButton>
+          {prop.rBtnText && <RightButton>{prop.rBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
+};
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: ${(prop) => `url("/images/${prop.bgImg}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, ${(prop) => (prop.rBtn ? "0.9" : "1")});
+  opacity: ${(prop) => (prop.rBtn ? "0.9" : "1")};
+  color: white;
+  text-transform: capitalize;
+  width: 256px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin: 10px;
+`;
+
+const RightButton = styled(LeftButton)`
+  color: black;
+  background-color: white;
+  opacity: 0.7;
+`;
+
+const Buttons = styled.div``;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
